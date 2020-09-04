0268d8c5cf5a5dbe198adf20e92e21696f238d7e
SudeP <acan.d@outlook.com>
2020-09-04 19:21:36 +0300

    .

--- Tool/SoupRecipe.cs
+++ Tool/SoupRecipe.cs
@@ -195,16 +195,6 @@ namespace PDK.Tool
                 throw new ArgumentOutOfRangeException("string.Legth > 0 required");
         }
     }
-    public static class SoupRecipeExtensions
-    {
-        public static SoupRecipe soupRecipe = new SoupRecipe();
-        public static string Salt;
-        public static void SetSalt(string salt) => Salt = salt;
-        public static string Stir(this string pureSoup) => pureSoup.Stir(Salt);
-        public static string Stir(this string pureSoup, string salt) => soupRecipe.Stir(pureSoup, salt);
-        public static string Fractionate(this string impureSoup) => impureSoup.Fractionate(Salt);
-        public static string Fractionate(this string impureSoup, string salt) => soupRecipe.Fractionate(impureSoup, salt);
-    }
     internal class Const
     {
         public readonly int binarySystemCode = 2;
---
+++ Tool/SoupRecipeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using PDK.Tool;
+
+public static class SoupRecipeExtensions
+{
+    public static SoupRecipe soupRecipe = new SoupRecipe();
+    public static string Salt;
+    public static void SetSalt(string salt) => Salt = salt;
+    public static string Stir(this string pureSoup) => pureSoup.Stir(Salt);
+    public static string Stir(this string pureSoup, string salt) => soupRecipe.Stir(pureSoup, salt);
+    public static string Fractionate(this string impureSoup) => impureSoup.Fractionate(Salt);
+    public static string Fractionate(this string impureSoup, string salt) => soupRecipe.Fractionate(impureSoup, salt);
+}
