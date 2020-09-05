1d899daf62dc58653a6722355c5905c9b2939377
SudeP <acan.d@outlook.com>
2020-09-05 13:30:22 +0300

    SoupRecipeExtension ref Number indexRange Error fixed

---
+++ TestApplication/SRTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApplication
+{
+    public partial class SRTest : Form
+    {
+        public SRTest()
+        {
+            InitializeComponent();
+        }
+
+        private void SRTest_Load(object sender, EventArgs e)
+        {
+            SoupRecipeExtensions.SetSalt("qwerty");
+            string pureJson = @"{""Id"":1000,""ModifiedDate"":""2020-09-04T13:01:34.94"",""IsActive"":5,""Name"":""Ahmet Can"",""Surname"":""Duyar"",""Username"":""ahmet"",""Password"":""1234""}";
+            var impureJson = pureJson.Stir();
+            var tempPureJson = impureJson.Fractionate();
+        }
+    }
+}
--- Tool/SoupRecipeExtensions.cs
+++ Tool/SoupRecipeExtensions.cs
@@ -2,11 +2,26 @@
 
 public static class SoupRecipeExtensions
 {
-    public static SoupRecipe soupRecipe = new SoupRecipe();
     public static string Salt;
     public static void SetSalt(string salt) => Salt = salt;
-    public static string Stir(this string pureSoup) => pureSoup.Stir(Salt);
-    public static string Stir(this string pureSoup, string salt) => soupRecipe.Stir(pureSoup, salt);
-    public static string Fractionate(this string impureSoup) => impureSoup.Fractionate(Salt);
-    public static string Fractionate(this string impureSoup, string salt) => soupRecipe.Fractionate(impureSoup, salt);
+    public static string Stir(this string pureSoup)
+    {
+        SoupRecipe soupRecipe = new SoupRecipe();
+        return soupRecipe.Stir(pureSoup, Salt);
+    }
+    public static string Stir(this string pureSoup, string salt)
+    {
+        SoupRecipe soupRecipe = new SoupRecipe();
+        return soupRecipe.Stir(pureSoup, salt);
+    }
+    public static string Fractionate(this string impureSoup)
+    {
+        SoupRecipe soupRecipe = new SoupRecipe();
+        return soupRecipe.Fractionate(impureSoup, Salt);
+    }
+    public static string Fractionate(this string impureSoup, string salt)
+    {
+        SoupRecipe soupRecipe = new SoupRecipe();
+        return soupRecipe.Fractionate(impureSoup, salt);
+    }
 }
